db11cce6c88b6d39db27db6e1af27e8c906f0ddb
Maxime Laroche <38856738+MaximeLaroche@users.noreply.github.com>
2021-01-28 20:54:34 -0500

    Commencé l'interface avec l'utilisateur

--- LAB1/Serveur/src/serveur.java
+++ LAB1/Serveur/src/serveur.java
@@ -5,17 +5,53 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.util.*;
+
 
 public class serveur {
 	
 	private static ServerSocket listener;
+	
+	
+	private static boolean isIP(String ip) {
+		if(ip.contains("^[a-zA-Z]+")) {
+			//reste beaucoup de contraintes à coder
+			return false;
+		}
+		return true;
+	}
+	private static boolean isPort(int port) {
+		if(5000<=port && port <= 5050)
+			return true;
+		return false;
+	}
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		int clientNumber = 0;
 		
-		String serverAddress = "127.0.0.1";
-		int serverPort = 5000;
+		
+		//Demander les info de mande à l'utilisateur
+		Scanner sc = new Scanner(System.in);
+		
+		boolean isIP = false;
+		String serverAddress = "abcd";
+		while(!isIP) {
+			System.out.println("Veuillez entrer l'addresse IP du serveur");
+			serverAddress = sc.next();
+			isIP = isIP(serverAddress);
+		}
+		
+		System.out.println("Veuillez entrer le no de client");
+		int clientNumber = sc.nextInt();
+		
+		
+		int serverPort = 0;
+		boolean isPort = false;
+		while(!isPort) {
+			System.out.println("Veuillez entrer le port du serveur");
+			serverPort = sc.nextInt();
+			isPort = isPort(serverPort);
+		}
 		
 		//Création de la connexion pour communiquer avec
 		listener = new ServerSocket();
