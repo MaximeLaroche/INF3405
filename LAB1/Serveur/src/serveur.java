0f84cd710b5ca1c53627a5a5d49f5183f737bf5c
Maxime Laroche <38856738+MaximeLaroche@users.noreply.github.com>
2021-01-28 19:02:44 -0500

    Ajouté les imports

---
+++ LAB1/Serveur/src/serveur.java
@@ -0,0 +1,17 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class serveur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("allo");
+	}
+
+	
+}
