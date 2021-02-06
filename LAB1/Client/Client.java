37261a4fe4b22353544bd8fa9039b3a1f56580a8
Zaka12345 <zakaria.babahadji@gmail.com>
2021-02-06 17:25:12 -0500

    implémentation des vérification client

--- LAB1/Client/Client.java
+++ LAB1/Client/Client.java
@@ -11,22 +11,18 @@ import java.util.Scanner;
 public class Client {
 	
 	private static Socket socket;
-	private static Scanner imput = new Scanner(System.in);
+	private static Scanner keyboard = new Scanner(System.in);
+	private static String ip = "";
+	private static int serverPort =0;
 	public static void main(String[] args) throws Exception
 	{
-		String serverAddres= "";
-		int port =0;
+		String serverAddress= "";
 		boolean isIp= false;
-		/*do {
-			System.out.println("Entrez l'adresse IP ex :127.0.0.1  :" );
-			serverAddres= imput.next();
-			isIp(serverAddress);
-		}while()*/
 		
-		System.out.println("Entrez l'adresse IP ex :127.0.0.1  :" );
-		System.out.println("Entrez un numéro de port entre 5000 et 5050" );
-		socket = new Socket(serverAddress,port);
-		System.out.format("Le serveur fonctionne sur :", serverAddress, port);
+		
+		askAddress();
+		socket = new Socket(serverAddress,serverPort);
+		System.out.format("Le serveur fonctionne sur :", serverAddress, serverPort);
 		
 		
 		DataInputStream in = new DataInputStream(socket.getInputStream());
@@ -39,6 +35,62 @@ public class Client {
 	}
 	
 	//Verifier bonne adresse IP
-	
+private static void askAddress() {
+		
+		boolean hasValidAddress = false;
+		
+		
+		String badIP = "Veuillez entrer des nombres entre 0 et 255 separés par un .\n Par exemple, 152.0.54.254";
+		String badPort = "Veuillez entrer uniquement un nombre entre 5000 et 5050 pour le port";
+		
+		
+		
+		do {
+			
+			hasValidAddress = true;
+			String ligne;
+			//Separer les octets par "." et le port par ":"
+			String entrees[] = {};		
+			do{
+				System.out.println("Veuiller entrer votre addresse IP, compose de quatre entier entre 0 et 255,\n suivi du port, entre 5000 et 5050.\n Par exemple, 127.0.50.254:5043 est une addresse valide.");
+			    ligne = keyboard.nextLine();
+				entrees = ligne.split("[:\\.]");
+				
+			}while(entrees.length != 5);
+			
+			//Valider l'addresse IP
+			
+			for(int i=0;i<4;i++) {
+				int temp;
+				try {
+					temp = Integer.parseInt(entrees[i]);
+					if(temp<0 || temp >255) {
+						System.out.println(badIP);
+						hasValidAddress = false;
+					}
+				}catch(Exception e) {
+					System.out.println(badIP);
+					hasValidAddress = false;
+				}
+			}
+			
+			//Valider le port
+			try {
+				int temp = Integer.parseInt(entrees[4]);
+				if(temp < 5000 || temp > 5050) {
+					System.out.println(badPort);
+					hasValidAddress = false;
+				}
+			}catch(Exception e) {
+			System.out.println(badPort);
+			hasValidAddress = false;
+			}
+			ip = entrees[0] + "." + entrees[1] + "." + entrees[2] + "." + entrees[3];
+			serverPort = Integer.parseInt(entrees[4]);
+						
+				
+				
+				}while(!hasValidAddress);
+			}
 	
 }
