44998b657e2a5179f3fb63b79923b425948199a0
Yun Ji Liao <60011714+yuliaaPoly@users.noreply.github.com>
2021-02-25 19:06:52 -0500

    add download

--- LAB1/Client/Client.java
+++ LAB1/Client/Client.java
@@ -1,26 +1,173 @@
 import java.io.DataInputStream;
-import java.io.DatainputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.*;
+import java.util.Scanner;
 
 public class Client {
-	
+
 	private static Socket socket;
+	private static Scanner keyboard = new Scanner(System.in);
+	private static String ip = "";
+	private static int port = 0;
+
+	public static void main(String[] args) throws Exception {
+		// askAddress();
+		ip = "127.0.0.1";
+		port = 5000;
+
+		socket = new Socket(ip, port);
+
+		while (true) {
+			System.out.format("Le serveur fonctionne sur :", ip, port, "\n");
+
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+			String helloMessageFromServer = in.readUTF();
+			System.out.println(helloMessageFromServer);
+
+			String command = sendCommand();
+			out.writeUTF(command);
+			if (command.contains("download")) {
+				String fileName = command.split(" ", 2)[1];
+				
+				download(fileName, socket);
+			} else if (command.contains("upload")) {
+				
+				String fileName = command.split(" ", 2)[1];
+				upload(fileName, socket);
+
+			}
+
+			try {
+				String response = in.readUTF();
+				System.out.println(response);
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+
+		}
+		// TODO uniquement close le socket quand on veut exit
+
+	}
+
+	private static void upload(String fileName, Socket socket) {
+
+		File file = new File(fileName);
+		DataOutputStream out = null;
+		if (file.exists()) {
+
+			long fileSize = file.length();
+
+			try {
+				FileInputStream fileIn = new FileInputStream(file);
+				out = new DataOutputStream(socket.getOutputStream());
+				out.writeLong(fileSize);
+				int paquetSize;
+				byte[] paquet = new byte[1024];
+				while (fileSize > 0) {
+					paquetSize = fileIn.read(paquet);
+					out.write(paquet, 0, paquetSize);
+					fileSize -= paquetSize;
+				}
+				fileIn.close();
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+
+		}
+	}
+	private static void download(String fileName, Socket socket) {
+		FileOutputStream fileOut = null;
+		 File currentDirectory = new File(System.getProperty("user.dir"));;
+		try {
+			fileName = currentDirectory.getPath() + "/" + fileName;
+			fileOut = new FileOutputStream(fileName);
+
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			long fileSize = in.readLong();
+			byte[] paquet = new byte[1024];
+			int paquetLenght;
+			while (fileSize > 0) {
+				paquetLenght = in.read(paquet);
+				fileOut.write(paquet, 0, paquetLenght);
+				fileSize -= paquetLenght;
+			}
+			fileOut.close();
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+
+
 	
-	public static void main(String[] args) throws Exception
-	{
-		String serverAddress = "127.0.0.1";
-		int port= 5000;
-		socket = new Socket(severAddress,port);
-		System.out.format("Le serveur fonctionne sur :", serverAddress, port);
-		
-		
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-		
-		String helloMessageFromServer= in.readUTF();
-		Systeme.out.println("HelloFromServer");
-		
-		socket.close();
-		
 	}
 	
+	private static String sendCommand() {
+		System.out.println("Envoyer une commande au serveur");
+		return keyboard.nextLine();
+	}
+
+	// Verifier bonne adresse IP
+	private static void askAddress() {
+
+		boolean hasValidAddress = false;
+
+		String badIP = "Veuillez entrer des nombres entre 0 et 255 separ�s par un .\n Par exemple, 152.0.54.254";
+		String badPort = "Veuillez entrer uniquement un nombre entre 5000 et 5050 pour le port";
+
+		do {
+
+			hasValidAddress = true;
+			String ligne;
+			// Separer les octets par "." et le port par ":"
+			String entrees[] = {};
+			do {
+				System.out.println(
+						"Veuiller entrer votre addresse IP, compose de quatre entier entre 0 et 255,\n suivi du port, entre 5000 et 5050.\n Par exemple, 127.0.50.254:5043 est une addresse valide.");
+				ligne = keyboard.nextLine();
+				entrees = ligne.split("[:\\.]");
+
+			} while (entrees.length != 5);
+
+			// Valider l'addresse IP
+
+			for (int i = 0; i < 4; i++) {
+				int temp;
+				try {
+					temp = Integer.parseInt(entrees[i]);
+					if (temp < 0 || temp > 255) {
+						System.out.println(badIP);
+						hasValidAddress = false;
+					}
+				} catch (Exception e) {
+					System.out.println(badIP);
+					hasValidAddress = false;
+				}
+			}
+
+			// Valider le port
+			try {
+				int temp = Integer.parseInt(entrees[4]);
+				if (temp < 5000 || temp > 5050) {
+					System.out.println(badPort);
+					hasValidAddress = false;
+				}
+			} catch (Exception e) {
+				System.out.println(badPort);
+				hasValidAddress = false;
+			}
+			ip = entrees[0] + "." + entrees[1] + "." + entrees[2] + "." + entrees[3];
+			port = Integer.parseInt(entrees[4]);
+
+		} while (!hasValidAddress);
+	}
+
 }
--- LAB1/Serveur/src/serveur.java
+++ LAB1/Serveur/src/serveur.java
@@ -1,81 +1,315 @@
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Vector;
 
+//import org.graalvm.compiler.lir.alloc.lsra.LinearScanAssignLocationsPhase;
 
-public class serveur {
-	
+import java.util.*;
+
+public class serveur{
+
 	private static ServerSocket listener;
+	private static String ip = "";
+	private static int port = 1;
+	private static Scanner keyboard = new Scanner(System.in);
 
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		int clientNumber = 0;
-		
-		String serverAddress = "127.0.0.1";
-		int serverPort = 5000;
-		
-		//Création de la connexion pour communiquer avec
-		listener = new ServerSocket();
-		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		
-		
-		//Association de l'addresse et du port `ala connexion
-		listener.bind(new InetSocketAddress(serverIP,serverPort));
-		
-		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
-		
-		try
-		{
-		
-			while(true)
-			{
-				new ClientHandler(listener.accept(),clientNumber++).start();
-			
+	private void askAddress() {
+
+		boolean hasValidAddress = false;
+
+		String badIP = "Veuillez entrer des nombres entre 0 et 255 separ?s par un .\n Par exemple, 127.0.0.1";
+		String badPort = "Veuillez entrer uniquement un nombre entre 5000 et 5050 pour le port";
+
+		do {
+
+			hasValidAddress = true;
+			String ligne;
+			// Separer les octets par "." et le port par ":"
+			String entrees[] = {};
+			do {
+				System.out.println(
+						"Veuiller entrer votre addresse IP, compose de quatre entier entre 0 et 255,\n suivi du port, entre 5000 et 5050.\n Par exemple, 127.0.0.1:5043 est une addresse valide.");
+				ligne = keyboard.nextLine();
+				entrees = ligne.split("[:\\.]");
+
+			} while (entrees.length != 5);
+
+			// Valider l'addresse IP
+
+			for (int i = 0; i < 4; i++) {
+				int temp;
+				try {
+					temp = Integer.parseInt(entrees[i]);
+					if (temp < 0 || temp > 255) {
+						System.out.println(badIP);
+						hasValidAddress = false;
+					}
+				} catch (Exception e) {
+					System.out.println(badIP);
+					hasValidAddress = false;
+					e.getStackTrace();
+				}
 			}
-		
-		}
-		finally
-		{
+
+			// Valider le port
+			try {
+				int temp = Integer.parseInt(entrees[4]);
+				if (temp < 5000 || temp > 5050) {
+					System.out.println(badPort);
+					hasValidAddress = false;
+				}
+			} catch (Exception e) {
+				System.out.println(badPort);
+				hasValidAddress = false;
+				e.getStackTrace();
+			}
+			ip = entrees[0] + "." + entrees[1] + "." + entrees[2] + "." + entrees[3];
+			port = Integer.parseInt(entrees[4]);
+
+		} while (!hasValidAddress);
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		// askAddress();
+		ip = "127.0.0.1";
+		port = 5000;
+
+		System.out.println("Veuillez entrer le no de client");
+		int clientNumber = 0;
+
+		// Cr?ation de la connexion pour communiquer avec
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIP = InetAddress.getByName(ip);
+
+		// Association de l'addresse et du port `ala connexion
+
+		listener.bind(new InetSocketAddress(serverIP, port));
+		System.out.format("The server is running on %s:%d%n", serverIP, port, "\n");
+
+		try {
+
+			while (true) {
+
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+
+		} finally {
 			listener.close();
 		}
 	}
-		
-		private static class ClientHandler extends Thread
-		{
-			private Socket socket;
-			private int clientNumber;
-			
-			public ClientHandler(Socket socket, int clientNumber)
-			{
-				this.socket = socket;
-				this.clientNumber = clientNumber;
-				System.out.println("New connection with client#"+ clientNumber + " at "+ socket);
-				
-			}
-			public void run()
-			{
-				try
-				{
-					DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-					
-					out.writeUTF("Hello from server - you are client# "+ clientNumber);
-					
-				}catch (IOException e)
-				{
-					System.out.println("Error handling client#" + clientNumber + ": " + e);
-				}
-				
-				
-				System.out.println("Connection with client# " + clientNumber + " closed");
+
+	private static class ClientHandler extends Thread {
+		private Socket socket;
+		private int clientNumber;
+		private boolean exit = false;
+		private File currentDirectory;
+
+		public ClientHandler(Socket socket, int clientNumber) {
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+			System.out.println("New connection with client#" + clientNumber + " at " + socket);
+
+			if (currentDirectory == null) {
+				currentDirectory = new File(System.getProperty("user.dir"));
+				currentDirectory = new File("entrepotServeur");
 			}
 		}
-		
+
+		public void run() {
+
+			try {
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				while (exit == false) {
+
+					out.writeUTF("Hello from server - you are client# " + clientNumber);
+
+					String query = in.readUTF();
+
+					printInfo(query);
+
+					String message = commands(query, socket);
+
+					out.writeUTF(message);
+
+				}
+			} catch (IOException e) {
+				System.out.println("Error handling client#" + clientNumber + ": " + e);
+				e.getStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (Exception e) {
+					System.out.print("Could not close the socket\n");
+					e.getStackTrace();
+				}
+			}
+
+		}
+
+		private void printInfo(String command) {
+			System.out.println("[" + ip + ":" + port + " - " + getDate() + "]: " + command);
+		}
+
+		private String getDate() {
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd @ HH:mm:ss");
+			return formatter.format(new Date());
+		}
+
+		// here
+
+		private String commands(String entrees, Socket socket) {
+			/*
+			 * Commandes[0] est la commande le reste des index sont les argument
+			 */
+
+			String[] commandes = entrees.split(" ", 2);
+			String output = "";
+			switch (commandes[0]) {
+			case "cd":
+				output = cd(commandes[1]);
+				break;
+			case "ls":
+				output = ls();
+				break;
+			case "mkdir":
+				output = mkdir(commandes[1]);
+				break;
+			case "upload":
+				output = upload(commandes[1]);
+				break;
+			case "download":
+					download(commandes[1]);
+				break;
+			case "exit":
+				output = "Client exited the server. Socket closed";
+				exit = true;
+				break;
+			default:
+				System.out.println("Commande invalide");
+			}
+			return output;
+
+		}
+
+		private String cd(String whereTo) {
+			String message;
+			/*
+			 * boolean directoryExists = false; String files[] = currentDirectory.list();
+			 * currentDirectory.e
+			 */
+
+			File temp = new File(whereTo);
+			currentDirectory = temp;
+			// TODO cette section s'exécute bizzarement
+			if (temp.exists()) {
+
+				message = "Moved to " + whereTo + "\n";
+			} else {
+				message = "Impossible to move to " + whereTo + ". The directory does not exist\n";
+			}
+
+			System.out.println(message);
+			return message;
+		}
+
+		private String ls() {
+
+			String files[] = currentDirectory.list();
+			String message = "";
+			for (String file : files) {
+				System.out.println(file);
+				message += file + "\n";
+			}
+			if (files.length == 0) {
+				message = "Directory is empty.\n";
+			}
+			return message;
+		}
+
+		private String mkdir(String directoryName) {
+			System.out.println("On est dans mkdir");
+			String message = "";
+			File folder = new File(currentDirectory.getPath() + "/" + directoryName);
+			if (folder.mkdir()) {
+				message = "votre dossier " + directoryName + " a ete cree";
+				System.out.println(message);
+				// out.writeUTF(message);
+			}
+			return message;
+
+		}
+
+		private String upload(String fileName) {
+
+			FileOutputStream fileOut = null;
+			String message = "Could not transfer file";
+			try {
+				fileName = currentDirectory.getPath() + "/" + fileName;
+				fileOut = new FileOutputStream(fileName);
+
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				long fileSize = in.readLong();
+				byte[] paquet = new byte[1024];
+				int paquetLenght;
+				while (fileSize > 0) {
+					paquetLenght = in.read(paquet);
+					fileOut.write(paquet, 0, paquetLenght);
+					fileSize -= paquetLenght;
+				}
+				fileOut.close();
+
+				message = "Succesfully tranfered file";
+
+			} catch (Exception e) {
+				System.out.println(message);
+				e.getStackTrace();
+			}
+
+			return message;
+		}
+
+		private String download(String fileName) {
+			String message = "downloading 99%... aka(failed)";
+			File file = new File("entrepotServeur/" + fileName);
+			DataOutputStream out = null;
+			if (file.exists()) {
+
+				long fileSize = file.length();
+
+				try {
+					FileInputStream fileIn = new FileInputStream(file);
+					out = new DataOutputStream(socket.getOutputStream());
+					out.writeLong(fileSize);
+					int paquetSize;
+					byte[] paquet = new byte[1024];
+					while (fileSize > 0) {
+						paquetSize = fileIn.read(paquet);
+						out.write(paquet, 0, paquetSize);
+						fileSize -= paquetSize;
+					}
+					fileIn.close();
+					message = "File download successfully";
+				} catch (Exception e) {
+					e.getStackTrace();
+				}
+
+			}
+			return message;
+		}
 	}
-	
 
 
-	
-
+}
